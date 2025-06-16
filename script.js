e21984a95ed3dead38e3b6ffabfca02e54eb2b85
Mariem Belghouthi <mariem.belghouthi@insat.ucar.tn>
2025-06-16 19:50:54 +0100

    Updated About Me section with image, text, CSS layout

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Mimi's Portfolio is alive!");
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
